560fdf38845078cee70f9edf6af5fe5de4e29bac
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-24 10:43:19 +1100

    Create action creators

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,28 @@
+// increment likes
+export function increment(index) {
+  // actions are just objects like so:
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+    // Same as `index: index`
+  }
+}
+
+// add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT'
+    postId,
+    author,
+    comment
+  }
+}
+
+// remove comment
+export function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT'
+    postId,
+    i
+  }
+}

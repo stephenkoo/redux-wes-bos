41980e713dbb018bf6bb7479f729c7f193e975a3
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-24 10:25:29 +1100

    Create redux store to store states

---
+++ store.js
@@ -0,0 +1,22 @@
+import {createStore, compose} from 'redux';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+// Import data from client/data
+import comments from './data/comments';
+import comments from './data/posts';
+
+// create an object for default data
+const defaultState = {
+  posts,
+  // Same as `posts: posts`
+  comments
+}
+
+
+const store = createStore(rootReducer, defaultState); // rootReducer is how to interface with Redux.
+export const history = syncHistoryWithStore(browserHistory, store); // put browserHistory into redux store
+export default store;

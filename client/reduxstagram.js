628cc037eb9b3c94289d1b2c1686bcd4d60f10fc
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-24 15:00:46 +1100

    Revert "Revert "integrate store with react router""
    This reverts commit a00edfb86cd69f520eea6ccfbf3101743ae84fac.

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -12,14 +12,18 @@ import PhotoGrid from './components/PhotoGrid';
 
 // import react router deps
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import store, {history} from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" components={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" components={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 // render(<p>hi</p>, document.getElementById('root'));
--- store.js
+++ store.js
@@ -1,4 +1,4 @@
-import {createStore, compose} from 'redux';
+import {createStore, compse} from 'redux';
 import {syncHistoryWithStore} from 'react-router-redux';
 import {browserHistory} from 'react-router';
 

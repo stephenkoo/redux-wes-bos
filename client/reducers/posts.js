20233777cd4aa658a9c1a439df204dc8031bc689
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-24 14:57:48 +1100

    Attempt to fix

--- client/reducers/posts.js
+++
@@ -1,8 +0,0 @@
-// a reducer takes in 2 things: the action (info about what happened) & copy of current state
-
-function posts(state = [], action) {
-  console.log(state, action);
-  return state;
-}
-
-export default posts;

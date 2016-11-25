47663368f8d496b333157f1e1c7c6427e3a00d5c
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-25 15:44:59 +1100

    display single photo component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
-const Single = props => (
-  <div className="single-photo">
-    I'm single
-  </div>
-);
+const Single = React.createClass({
+  render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    // console.log(post);
+    
+    // get us the post
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
+    )
+  }
+});
 
 export default Single;

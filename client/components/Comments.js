c094517acef8a823699bcf3f31cbe1a5f712aa7a
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-28 09:42:18 +1100

    Create add comments functionality

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -12,13 +12,32 @@ const Comments = React.createClass({
         </p>
       </div>
     )
-  }, 
+  },
+  handleSubmit (e) {
+    e.preventDefault(); // prevents event refreshing on submit
+    // console.log("Submitting form")
+    // console.log(this.refs)
+    const {postId} = this.props.params;
+    const author = this.refs.author.value; // this.refs.author is the html element, .value is the actual text inside
+    const comment = this.refs.comment.value;
+    // console.log(postId, author, comment);
+    // We're using the addComments reducer to update state in actionCreators.js
+    this.props.addComments(postId, author, comment);
+    // Change our comments reducer in reducers/comments.js to display it
+
+  },
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden />
+        </form>
       </div>    
     )
+    // refs used to grab content from form
   }
 });
 
--- client/components/Single.js
+++ client/components/Single.js
@@ -15,10 +15,11 @@ const Single = React.createClass({
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments postComments={postComments}/>
+        <Comments postComments={postComments} {...this.props}/>
       </div>
     )
   }
+  // <Commanets {...this.props}> passes along the props needed to Comments.js
 });
 
 export default Single;

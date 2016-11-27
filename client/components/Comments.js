da44aed6c5b794298ab68df38dbf655cad54972b
Stephen Koo <stephen.koo@inlight.com.au>
2016-11-28 09:26:17 +1100

    Display comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, i) { // Used to avoid having create 2nd component to load comments
+    // console.log(comment);
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  }, 
   render() {
     return (
-      <div className="comment">
-        I'm comments
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
       </div>    
     )
   }
--- client/components/Single.js
+++ client/components/Single.js
@@ -4,16 +4,18 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const {postId} = this.props.params;
     // index of the post
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[i];
     // console.log(post);
+    const postComments = this.props.comments[postId] || []; // If no comments, then blank []
     
     // get us the post
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
